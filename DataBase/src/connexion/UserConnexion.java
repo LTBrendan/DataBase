bed452b4aa959eb7e732080e3f1fd99db2658b39
Patate1er <Patate1er@10.2.139.202>
2017-06-12 16:29:01 +0200

    v1.0

---
+++ DataBase/src/connexion/UserConnexion.java
@@ -0,0 +1,28 @@
+package connexion;
+
+public class UserConnexion {
+	
+	private String adresse;
+	private String login;
+	private String password;
+	
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
