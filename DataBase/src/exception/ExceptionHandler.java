6dedee05bf3395de373686ac860bb58419610668
Patate1er <Patate1er@10.2.139.202>
2017-06-14 15:42:12 +0200

    v4.2

--- DataBase/src/exception/ExceptionHandler.java
+++ DataBase/src/exception/ExceptionHandler.java
@@ -1,7 +1,15 @@
 package exception;
 
+/**
+ * This class get error messages to simplify it
+ */
 public class ExceptionHandler {
 	
+	/**
+	 * This method analyse error message and cut the useless parts
+	 * @param message the error message
+	 * @return the simplified error message
+	 */
 	public static String analyse (String message) {
 		String ret = message;
 		String[] tab = ret.split(":\\s");
