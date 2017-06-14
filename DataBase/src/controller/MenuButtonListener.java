e2ca85148a2cbce147802af3862adb5f37626be2
mathisRoger <m.roger.mathis@gmail.com>
2017-06-14 15:04:17 +0200

    v4.3

--- DataBase/src/controller/MenuButtonListener.java
+++ DataBase/src/controller/MenuButtonListener.java
@@ -2,6 +2,7 @@ package controller;
 
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -38,13 +39,13 @@ public class MenuButtonListener implements MouseListener {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
 		
-		panel.add(new JLabel("Query : " + BaseDeDonnees.getTextPane().getText()));
+		panel.add(new JLabel("Query : " + BaseDeDonnees.getTextPane().getText()), BorderLayout.NORTH);
 		BaseDeDonnees.setPanel_1(panel);
 		
 		JScrollPane scrollPane = new JScrollPane(panel);
+		scrollPane.setPreferredSize(new Dimension(20,250));
 		BaseDeDonnees.setScrollPane(scrollPane);
 
 		BaseDeDonnees.getSplitPane().setLeftComponent(scrollPane);
 	}
-
 }
