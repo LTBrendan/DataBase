81ba261a151b074b761923a078318f551d2bbde3
mathisRoger <m.roger.mathis@gmail.com>
2017-06-16 23:55:11 +0200

    v6.61

---
+++ DataBase/src/vue/StatsPanel.java
@@ -0,0 +1,29 @@
+package vue;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class StatsPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private static StatsPanel statsPanel;
+	/**
+	 * Create the panel.
+	 */
+	public StatsPanel() {
+		
+		this.setBackground(new Color(Launcher.color, Launcher.color + 3, Launcher.color + 8));
+		
+		statsPanel = this;
+	}
+	
+	public static StatsPanel getStatsPanel() {
+		return statsPanel;
+	}
+
+}
