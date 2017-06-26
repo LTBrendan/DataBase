38305043af3aff806767132baa5de2478908bba3
Patate1er <Patate1er@10.1.0.115>
2017-06-26 09:19:25 +0200

    v7.43

--- DataBase/src/model/query/QueryExecutor.java
+++ DataBase/src/model/query/QueryExecutor.java
@@ -5,6 +5,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * This class allows to execute a query
+ */
 public class QueryExecutor {
 
 	// the database connection
@@ -16,31 +19,58 @@ public class QueryExecutor {
 	// the user using database
 	
 	
+	/**
+	 * The constructor
+	 * @param conn the database connection
+	 * @param statement the database statement
+	 */
 	public QueryExecutor (Connection conn, Statement statement) {
 		this.setConn(conn);
 		this.setStatement(statement);
 	}
 	
+	/**
+	 * This method execute a query
+	 * @param query the query to execute
+	 * @return the query ResultSet
+	 * @throws SQLException only if an exception occurs
+	 */
 	public ResultSet executeQuery(String query) throws SQLException {
 		resultat = statement.executeQuery(query);
 		return resultat;
 	}
 	
+	/**
+	 * Getter for the connection
+	 * @return the connection
+	 */
 	public Connection getConn() {
 		return conn;
 	}
 
 
+	/**
+	 * Setter for the connection
+	 * @param conn the new connection
+	 */
 	public void setConn(Connection conn) {
 		this.conn = conn;
 	}
 
 
+	/**
+	 * Getter for the statement
+	 * @return the tatement
+	 */
 	public Statement getStatement() {
 		return statement;
 	}
 
 
+	/**
+	 * Setter for the statement
+	 * @param statement the new statement
+	 */
 	public void setStatement(Statement statement) {
 		this.statement = statement;
 	}
